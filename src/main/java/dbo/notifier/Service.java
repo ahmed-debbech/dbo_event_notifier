3fad3dc95716e7d98499cc8c43744be92cd3b511
ahmed-debbech <debbech.ahmed@gmail.com>
2024-02-23 16:44:02 +0100

    fix time conversion

--- src/main/java/dbo/notifier/Service.java
+++ src/main/java/dbo/notifier/Service.java
@@ -7,15 +7,15 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cglib.core.Local;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @org.springframework.stereotype.Service
 public class Service {
@@ -26,7 +26,7 @@ public class Service {
     String htmlBody;
     List<Event> events;
 
-    Date nextNotif;
+    Date nextNotif = null;
 
     @Autowired
     private Logger out;
@@ -34,35 +34,116 @@ public class Service {
     public void run(){
         grab();
         jparse();
-        //least();
+        least();
         schedule();
     }
 
     private void schedule(){
-        nextNotif = new Date();
-        nextNotif.setSeconds(nextNotif.getSeconds() +10);
+        if(nextNotif == null) {
+            nextNotif = new Date();
+            nextNotif.setSeconds(nextNotif.getSeconds() + 10);
+        }
     }
+
     public void notifyUsers(){
+        if(nextNotif == null) return;
+
         RestTemplate restTemplate = new RestTemplate();
         String url = urlTelegram;
         try {
-            url += URLEncoder.encode("HEY", StandardCharsets.UTF_8.toString());
-            restTemplate.getForEntity(url , String.class);
+            Calendar date = new GregorianCalendar(2024, nextNotif.getMonth()+1, nextNotif.getDate());
+
+            String dateTime = date.get(Calendar.YEAR) + "-" + String.format("%02d", date.get(Calendar.MONTH)) + "-" +
+                    String.format("%02d", date.get(Calendar.DAY_OF_MONTH)) + "T"
+                    + String.format("%02d",nextNotif.getHours()) + ":" + String.format("%02d",nextNotif.getMinutes())
+                    + ":" + String.format("%02d",nextNotif.getSeconds());
+
+
+            LocalDateTime notifTime = LocalDateTime.parse(dateTime);
+            if(LocalDateTime.now().isAfter(notifTime)) {
+                url += URLEncoder.encode("HEY", StandardCharsets.UTF_8.toString());
+                //restTemplate.getForEntity(url, String.class);
+                nextNotif = null;
+            }
         } catch (UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
     }
-    private void least(){
+
+    private List<Date> getByCountdown(){
+        List<Date> d = new ArrayList<>();
+
         for(Event ev : events){
+            Date r = new Date();
             int count = Integer.parseInt(ev.getCountdown().split(" ")[0]);
             String countUnit = ev.getCountdown().split(" ")[1].substring(0,2);
-            if(countUnit.equals("day")) {
-                System.err.println("day");
+
+            if(countUnit.equals("da")) {
+                r.setDate(r.getDate() + count);
             }
-            if(countUnit.equals("day")) {
-                System.err.println("day");
+            if(countUnit.equals("ho")) {
+                r.setHours(r.getHours() + count);
+            }
+            if(countUnit.equals("mi")) {
+                r.setMinutes(r.getMinutes() + count);
+            }
+            if(countUnit.equals("se")) {
+                r.setSeconds(r.getSeconds() + count);
+            }
+            d.add(r);
+        }
+        return d;
+    }
+
+    private Date parseTime(String time, Date g){
+        String h = "";
+        String m = "";
+        int s = 0;
+        int hn = 0;
+        int mn = 0;
+
+        if(time.contains("am")){
+            if(!time.contains(":")){
+                h += time.substring(0,time.length()-2);
+                hn = Integer.parseInt(h);
+            }else{
+                h+= time.split(":")[0];
+                m+=time.split(":")[1].substring(0, time.split(":")[1].length()-2);
+                hn = Integer.parseInt(h) ;
+                mn = Integer.parseInt(m);
             }
         }
+        if(time.contains("pm")){
+            if(!time.contains(":")){
+                h += time.substring(0,time.length()-2);
+                hn = Integer.parseInt(h) + 12;
+            }else{
+                h+= time.split(":")[0];
+                m+=time.split(":")[1].substring(0, time.split(":")[1].length()-2);
+                hn = Integer.parseInt(h) + 12;
+                mn = Integer.parseInt(m);
+            }
+        }
+        g.setHours(hn);
+        g.setMinutes(mn);
+        g.setSeconds(s);
+        return g;
+    }
+    private List<Date> getBySchedule(List<Date> d){
+        int i = 0;
+        for(Event ev : events){
+            String time = ev.getSchedule();
+            Date dd = parseTime(time, d.get(i));
+            d.set(i, dd);
+            i++;
+        }
+        return d;
+    }
+
+    private void least(){
+        List<Date> d = getByCountdown();
+        d = getBySchedule(d);
+        System.err.println(d);
     }
 
     private void jparse(){
