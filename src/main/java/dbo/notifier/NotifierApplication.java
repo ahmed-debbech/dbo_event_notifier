54884004e7bcb43c2dca199c98abf622510a8692
ahmed-debbech <debbech.ahmed@gmail.com>
2024-02-24 15:21:55 +0100

    increment min time

--- src/main/java/dbo/notifier/NotifierApplication.java
+++ src/main/java/dbo/notifier/NotifierApplication.java
@@ -20,7 +20,7 @@ public class NotifierApplication {
 		SpringApplication.run(NotifierApplication.class, args);
 	}
 
-	@Scheduled(cron = "* */1 * * * *")
+	@Scheduled(cron = "* */5 * * * *")
 	private void scheduleWithCron() {
 		System.out.println("Cron Job is started at : " + LocalTime.now());
 		service.run();
