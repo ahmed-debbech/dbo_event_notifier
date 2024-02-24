296071db897570d9276634558553bf197a4a007a
ahmed-debbech <debbech.ahmed@gmail.com>
2024-02-24 15:32:31 +0100

    fix cron

--- src/main/java/dbo/notifier/NotifierApplication.java
+++ src/main/java/dbo/notifier/NotifierApplication.java
@@ -20,7 +20,7 @@ public class NotifierApplication {
 		SpringApplication.run(NotifierApplication.class, args);
 	}
 
-	@Scheduled(cron = "* */5 * * * *")
+	@Scheduled(cron = "*/50 * * * * *")
 	private void scheduleWithCron() {
 		System.out.println("Cron Job is started at : " + LocalTime.now());
 		service.run();
